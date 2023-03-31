4344d2e1281e552e5249d7d2c485a71c79c8d854
flat1ander <thematthewlane@gmail.com>
2023-03-31 11:59:01 -0400

    Delete route working. Edit route needs debugging.

--- controllers/animals.js
+++ controllers/animals.js
@@ -7,20 +7,42 @@ router.get('/', (req, res) => {
     res.render('index.ejs', {animals});
 })
 
-// Edit and Update Animal:
+// Show Route
+router.get('/:id', (req, res) => {
+    const animal = animals[req.params.id];
+    animal.id = req.params.id
+    res.render('show.ejs', {animal})
+})
+
+// New Route
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+// Post Route
+router.post('/', (req, res) => {
+    req.body.extinct = req.body.extinct === 'on' ? true : false
+    animals.unshift(req.body)
+    res.redirect('/animals')
+})
+
+// Edit and Update Routes:
 router.get('/:id/edit', (req, res) => {
     const animal = animals[req.params.id];
     res.render('edit.ejs', {animal})
 })
 
-// Show Route
-router.get('/:id', (req, res) => {
-    const animal = animals[req.params.id];
-    animal.id = req.params.id
-    console.log(animal)
-    res.render('show.ejs', {animal})
+router.put('/:id', (req, res) => {
+    const animal = animals[req.params.id]
+    animals.splice(animal, 1)
+    res.redirect('/animals')
 })
 
+// Delete Route
+router.delete('/:id', (req, res) => {
+    animals.splice(req.params.id, 1)
+    res.redirect('/animals')
+})
 
 
 
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000
 const animals = require('./models/animal')
-// const methodOverride = require('method-override')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended:false }));
 app.use(express.static('public'));
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
+app.use(bodyParser.json());
 
 const animalsController = require('./controllers/animals');
 app.use('/animals', animalsController)
